77a0ad3f80e37bde1b0154e5f141059c78b95f22
jasmine campbell <jasmine1089@gmail.com>
2019-03-30 00:13:58 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,9 +2,8 @@ def fizzbuzz (int)
  if int % 3 == 0 && int % 5 == 0 
  puts "FizzBuzz"
 elsif int % 3 == 0
-if int % 5 == 0
-  puts "Buzz"
-end
+puts "Fizz"
+elsif int  % 5 == 0
 if (int % 3 == 0) && (int % 5 == 0)
   puts "FizzBuzz"
 end
